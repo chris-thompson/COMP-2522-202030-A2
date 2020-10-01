3de10c1b2cacd88ee0a3535f50084164969983ab
Chris Thompson <cthompson98@bcit.ca>
2020-09-30 23:15:01 -0700

    Hello world!

---
+++ src/Driver.java
@@ -0,0 +1,19 @@
+/**
+ * Drives the program.
+ *
+ * @author YourName
+ * @version 2020
+ */
+public final class Driver {
+
+    private Driver() { }
+
+    /**
+     * Drives the program.
+     *
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+        // Your code goes here!
+    }
+}
